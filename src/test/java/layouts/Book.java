cccbd191edc423f907e0fe71ad37429ede123b7f
Paul Morrison <jpaulmorr@gmail.com>
2020-07-02 22:45:44 -0400

    Some folder restructuring﻿

---
+++ src/test/java/layouts/Book.java
@@ -0,0 +1,14 @@
+package layouts;
+
+import com.jpaulmorrison.jbdtypes.*;
+
+
+public class Book {
+	public int id;
+	public String title;
+	public String author;
+	//public BigDecimal price;
+	public MPrice price;
+	public int    qty;
+}
+
